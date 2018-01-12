3bec1a991e94db569001e096e1b611d5c39741e5
Ben Kraft <ben@benkraft.org>
2018-01-12 11:17:13 -0500

    Make it do stuff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import json
 import os
+import random
 import sys
 
 sys.path.insert(0, 'lib')
@@ -8,12 +10,44 @@ import flask
 
 app = flask.Flask(__name__)
 app.config.update(
-    DEBUG=os.environ.get('SERVER_SOFTWARE').startswith('Development'))
+    DEBUG=os.environ.get('SERVER_SOFTWARE', '').startswith('Development'))
+
+
+_EMOJI = None
+
+
+def _emoji():
+    global _EMOJI
+    if _EMOJI is None:
+        with open('emoji-data/emoji.json') as f:
+            _EMOJI = json.load(f)
+    return _EMOJI
+
+
+def character(emoji_datum):
+    return ''.join(unichr(int(c, 16))
+                   for c in emoji_datum['unified'].split('-'))
+
+
+def shortcode(emoji_datum):
+    return ':%s:' % random.choice(emoji_datum['short_names'])
+
+
+def team_name_data():
+    emoji = random.sample(_emoji(), 3)
+    return {
+        'chars': ''.join(character(e) for e in emoji),
+        'shortcodes': ' '.join(shortcode(e) for e in emoji),
+        'names': ' '.join(e['name'] for e in emoji),
+    }
 
 
 @app.route('/slash', methods=['POST'])
 def slash():
     return flask.jsonify({
         'response_type': 'in_channel',
-        'text': u'testing 123',
+        'text': (u'Our team name is %(chars)s '
+                 'which you can type like so: `%(shortcodes)s` '
+                 'or pronounce like so: %(names)s.'
+                 % team_name_data()),
     })
